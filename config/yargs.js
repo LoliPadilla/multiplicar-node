8077a3925f34f74b5282f6c55b49a972575ec7e3
Loli Padilla <lolilasaga@hotmail.com>
2019-01-04 17:04:10 -0300

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,19 @@
+const opts = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        default: 10,
+        alias: 'l'
+    }
+}
+
+const argv = require('yargs').command('listar', 'Imprime en la consola la tabla de multiplicar', opts)
+    .command('crear', 'Crear un archivo con la tabla de multiplicar', opts)
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
